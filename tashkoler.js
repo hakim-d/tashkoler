7a215b5a77405ad19be809a99e295994a7957610
Hakim DAHOUNE <hakim.dahoune@gmail.com>
2019-05-03 11:42:03 +0100

    Update tashkoler.js

--- tashkoler.js
+++ tashkoler.js
@@ -13,13 +13,26 @@ var tashkoler = (function (){
     style.innerHTML = '.tashkoler haraka { color: ' + color + '; font-size: ' + size + '; }';
     document.getElementsByTagName('head')[0].appendChild(style);
 
-    // Define the DOM target to apply the colored tashkeel.
+    /*
+    Define the DOM target to apply the colored tashkeel to.
+    Default target tag is "p"
+    */
     var bind = document.querySelector(target);
     bind.className += " tashkoler";
     var join;
     var text = document.querySelector(target).innerHTML;
-    text = text.replace(/ٌ|ّ|ٍ|ُ|َ|ِ|ً/gi, function checkStr(x,y){
+    
+    // Find all the harakat and wrap each one of them with <haraka></haraka>
+    text = text.replace(/ٌ|ّ|ٍ|ُ|َ|ِ|ً/gi, function wrapHaraka(x,y){
+      /*
+        x: the harakat
+        y: position of the haraka in the text
+        Wrapping the harakat with the tags, breaks the arabic letters joints
+        when the harakat is in the middle of a word (no space after or before: y+1 and y-1)
+        so for this case, this line adds a zero width joiner (&zwj;)
+        */
       join = (text.charAt(y+1) != " " && text.charAt(y-1) != " ") ? "&zwj;" : "";
+      // Wrap the harakat and add a zero width joiner if needed
       return join + "<haraka>" + x + "</haraka>" + join;
     });
 
